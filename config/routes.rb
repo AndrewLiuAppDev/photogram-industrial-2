1abb29db0c3adfbc2acfad2d3867872a00bd0182
yanyuliu92 <yliu33@chicagobooth.edu>
2021-04-21 09:03:39 +0000

    Finished users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "photos#index"
-  resources :users, only: :show
+  
   devise_for :users
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
+  get "/:username" => "users#show"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
